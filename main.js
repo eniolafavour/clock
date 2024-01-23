31c4c2c7fc3538515832e75dfb02695178a44141
eniolafavour <eniolafavour10@gmail.com>
2024-01-23 12:52:11 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let hour = document.getElementById('hour')
+let minute = document.getElementById('minute')
+let second = document.getElementById('second')
+
+function displayTime() {
+    let date = new Date()
+    let hh = date.getHours()
+    let mm = date.getMinutes()
+    let ss = date.getSeconds()
+    let hRotation = 30*hh + mm/2
+    let mRotation = 6*mm 
+    let sRotation = 6*ss
+    hour.style.transform = `rotate(${hRotation}deg)`
+    minute.style.transform = `rotate(${mRotation}deg)`
+    second.style.transform = `rotate(${sRotation}deg)`
+}
+
+setInterval(displayTime, 1000)
